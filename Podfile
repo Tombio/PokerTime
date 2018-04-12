568b5430f3442c4935c94791ba81fc7ba4021dcd
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2018-04-12 12:33:09 +0300

    Initial release

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'PokerTime' do
+  use_frameworks!
+  pod 'LCDView', :git => 'https://github.com/coniferprod/LCDView.git'
+end
--- PokerTime/ViewController.swift
+++ PokerTime/ViewController.swift
@@ -7,19 +7,105 @@
 //
 
 import UIKit
+import LCDView
 
 class ViewController: UIViewController {
-
+    
+    let levelTime = 12 * 60 // Seconds
+    let smallBlindStart = 25 // Big blind = 2 x small blind
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if paused {
+            reset()
+        }
+    }
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var currentLevel = 1 {
+        didSet {
+            smallBlindView.caption = String(format: "%4d", smallBlindStart * currentLevel)
+            bigBlindView.caption = String(format: "%4d", (smallBlindStart * 2) * currentLevel)
+        }
+    }
+    
+    var remainingTime = 0 {
+        didSet {
+            let minutes = remainingTime / 60
+            let seconds = remainingTime - minutes * 60
+            timerView.caption = String(format: "%02d", minutes) + (seconds % 2 == 0 ? ":" : " ") + String(format: "%02d", seconds)
+        }
+    }
+    var paused = true {
+        didSet {
+            resetButton.isHidden = !paused
+        }
+    }
+    
+    var timeDelta: Double = 0
+    
+    @IBOutlet weak var timerView: LCDView!
+    @IBOutlet weak var smallBlindView: LCDView!
+    @IBOutlet weak var bigBlindView: LCDView!
+    
+    private func reset() {
+        currentLevel = 1
+        remainingTime = levelTime
+        paused = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timerView.caption = "12:00"
+        smallBlindView.caption = "25"
+        bigBlindView.caption = "50"
+        addTapRecognizer()
+        reset()
+        
+        let displayLink = CADisplayLink(target: self, selector: #selector(update))
+        displayLink.add(to: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @objc private func update() {
+        let now = floor(Date().timeIntervalSince1970)
+        if now - timeDelta >= 1 {
+            timeDelta = now
+            if !paused {
+                remainingTime -= 1
+            }
+            
+            if remainingTime == 0 {
+                currentLevel = currentLevel * 2
+                remainingTime = levelTime
+            }
+        }
+        //debugPrint("time delta", timeDelta, "now", now)
+    }
+    
+    @objc private func togglePaused() {
+        paused = !paused
+    }
+    
+    func addTapRecognizer() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.togglePaused))
+        tap.cancelsTouchesInView = false
+        timerView.addGestureRecognizer(tap)
+    }
 }
-
